1ba7a81385445366745f7783448c8a8aafcdc634
gabrinetor <gabriela.netor94@gmail.com>
2020-10-24 19:19:43 -0300

    Roteamento

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import "bootstrap-4-react/dist/bootstrap-4-react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
 
-export default class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <ProductList />
-        <Details />
-        <Cart />
-        <Default />
-      </React.Fragment>
-    )
-  }
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/detalhes" component={Details} />
+          <Route path="/compras" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </div> 
+    </Router>
+  );
 }
+
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Barra do Menu</h3>
-            </div>
-        )
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to="/">
+                    <img src={logo} alt="Loja de Smartphones" className="navbar-brand" />
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to="/compras" className="ml-auto">
+                    <button>
+                        <i className="fas fa-cart-plus" />
+                    </button>
+                </Link>
+            </nav>
+        );
     }
 }
