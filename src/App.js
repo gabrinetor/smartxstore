d7852231cd6f65ab58cd505db79017c087636d3d
gabrinetor <gabriela.netor94@gmail.com>
2020-10-24 04:06:52 -0300

    componentizando

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import React, { Component } from 'react';
-import { Alert } from 'bootstrap-4-react';
+import "bootstrap-4-react/dist/bootstrap-4-react";
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Alert primary>Primeiro botão</Alert>
-        <Alert secondary>Segundo botão</Alert>
-        <span><i className="fas fa-home" /></span>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
+      </React.Fragment>
     )
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Barra do Menu</h3>
+            </div>
+        )
+    }
+}
